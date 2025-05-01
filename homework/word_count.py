f01bccb538682853c191184afb3794ec4a2f1a5c
Laura Ladino Gallego <lladinog@unal.edu.co>
2025-05-01 11:04:23 -0500

    Refactor file handling in word_count.py and update README for Python command

--- homework/word_count.py
+++ homework/word_count.py
@@ -21,8 +21,11 @@ from itertools import groupby
 def copy_raw_files_to_input_folder(n):
     """Funcion copy_files"""
 
-    if not os.path.exists("files/input"):
-        os.makedirs("files/input")
+    if os.path.exists("files/input"):
+        for file in glob.glob("files/input/*"):
+            os.remove(file)
+        os.rmdir("files/input")
+    os.makedirs("files/input")
 
     for file in glob.glob("files/raw/*"):
         for i in range(1, n + 1):
@@ -104,6 +107,7 @@ def mapper(sequence):
 #
 def shuffle_and_sort(sequence):
     """Shuffle and Sort"""
+    return sorted(sequence, key=lambda x: x[0])
 
 
 #
@@ -114,6 +118,10 @@ def shuffle_and_sort(sequence):
 #
 def reducer(sequence):
     """Reducer"""
+    result = []
+    for key, group in groupby(sequence, key=lambda x: x[0]):
+        result.append((key, sum(value for _, value in group)))
+    return result
 
 
 #
@@ -122,6 +130,12 @@ def reducer(sequence):
 #
 def create_ouptput_directory(output_directory):
     """Create Output Directory"""
+    if os.path.exists(output_directory):
+        for file in glob.glob(f"{output_directory}/*"):
+            os.remove(file)
+        os.rmdir(output_directory)
+    os.makedirs(output_directory)
+
 
 
 #
@@ -134,6 +148,9 @@ def create_ouptput_directory(output_directory):
 #
 def save_output(output_directory, sequence):
     """Save Output"""
+    with open(f"{output_directory}/part-00000", "w", encoding="utf-8") as f:
+        for key, value in sequence:
+            f.write(f"{key}\t{value}\n")
 
 
 #
@@ -142,6 +159,8 @@ def save_output(output_directory, sequence):
 #
 def create_marker(output_directory):
     """Create Marker"""
+    with open(f"{output_directory}/_SUCCESS", "w", encoding="utf-8") as f:
+        f.write("")
 
 
 #
@@ -151,6 +170,12 @@ def run_job(input_directory, output_directory):
     """Job"""
     sequence = load_input(input_directory)
     sequence = line_preprocessing(sequence)
+    sequence = mapper(sequence)
+    sequence= shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    create_ouptput_directory(output_directory)
+    save_output(output_directory, sequence)
+    create_marker(output_directory)
 
 if __name__ == "__main__":
 
